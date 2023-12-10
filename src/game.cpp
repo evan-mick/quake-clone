f77fa52065bb6ea8f8939032dcb7184d6d0f7181
evan-mick <evsammick@gmail.com>
2023-12-09 21:04:23 -0500

    working input sys

--- src/game.cpp
+++ src/game.cpp
@@ -91,10 +91,11 @@ void Game::startGame(bool server) {
     Renderer render = Renderer(&cam);
     Renderer::default_render->setRatio(xscale, yscale);
 
-    entity_t ent = ecs.createEntity({ FLN_TRANSFORM, FLN_PHYSICS, FLN_TEST, FLN_RENDER });
+    entity_t ent = ecs.createEntity({ FLN_TRANSFORM, FLN_PHYSICS, FLN_TEST, FLN_RENDER, FLN_INPUT });
     Renderable* rend = static_cast<Renderable*>(ecs.getComponentData(ent, FLN_RENDER));
-//    rend->model_id = static_cast<uint8_t>(PrimitiveType::PRIMITIVE_SPHERE);
-    rend->model_id = 5;
+    rend->model_id = static_cast<uint8_t>(PrimitiveType::PRIMITIVE_SPHERE);
+//    rend->model_id = 5;
+    registerInputs();
 
     Transform* trans = static_cast<Transform*>(ecs.getComponentData(ent, FLN_TRANSFORM));
     trans->pos = glm::vec3(-1.f, -1.f, 0);
@@ -104,9 +105,15 @@ void Game::startGame(bool server) {
 //    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
 //        Renderer::default_render->resizeGL(width, height);
 //    });
+    glfwSetKeyCallback(window, Input::key_callback);
 
     while (m_running) {
 
+        Input::checkKeys(window);
+        if (Input::getHeld())
+            std::cout << "held " << Input::getHeld() << std::endl;
+
+        getComponentData<InputData>(&ecs, ent, FLN_INPUT)->dat = Input::getHeld();
 //        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         render.startDraw();
 
@@ -169,6 +176,12 @@ void Game::registerECSSystems(ECS& ecs, Physics& phys, Renderer& renderer) {
         PhysicsData* phys = getPhys(e, ent);
         Transform* trans = getTransform(e, ent);
         Test* ts = getComponentData<Test>(e, ent, FLN_TEST);
+        InputData* in = getComponentData<InputData>(e, ent, FLN_INPUT);
+        std::cout << "test " << ent << std::endl;
+
+        if (Input::isHeld(in->dat, IN_FORWARD)) {
+            std::cout << "hi" << std::endl;
+        }
 
         ts->timer += delta;
 
--- src/input.h
+++ src/input.h
@@ -2,6 +2,7 @@
 #define INPUT_H
 #include "glfw/glfw3.h"
 #include "game_types.h"
+#include <iostream>
 
 class Input
 {
@@ -9,12 +10,35 @@ public:
     Input();
 
     static inline void registerHeld(int key, int inputBitFlag) {
-        if (inputBitFlag >= 0 && inputBitFlag < sizeof(input_t))
+        if (inputBitFlag >= 0 && inputBitFlag < (sizeof(input_t) * 8)) {
             keys[inputBitFlag] = key;
+            std::cout << "registerd " << key << " " << inputBitFlag << std::endl;
+        }
     }
 
     static inline input_t getHeld() {
         return held;
+    }
+
+    static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+//        std::cout << "callback " << key << " " << action << std::endl;
+        for (int i = 0; i < keys.size(); i++) {
+            if (keys[i] == 0 || key != keys[i])
+                continue;
+
+            int state = action;
+
+            input_t held_change = -1;
+            held_change = held_change & (1 << i);
+
+            if (state == GLFW_PRESS){
+                held |= held_change;
+            } else if (state == GLFW_RELEASE) {
+                held &= ~(i << i);
+            }
+            std::cout << (int)held << std::endl;
+        }
     }
 
     static inline bool isHeld(int inputBitFlag) {
